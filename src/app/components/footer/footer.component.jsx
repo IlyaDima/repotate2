cbb6cb28e3088476d3764913a5d229852879ff75
id-webdev <ilyuha45@gmail.com>
2023-01-15 11:19:50 +0100

    Footer: Changed the year

--- src/app/components/footer/footer.component.jsx
+++ src/app/components/footer/footer.component.jsx
@@ -1,50 +1,48 @@
-import React from 'react';
-import MediaQuery from 'react-responsive'
+import React from "react";
+import MediaQuery from "react-responsive";
 
-import './footer.styles.scss';
+import "./footer.styles.scss";
 
-import Social from '../header/social/social.component';
-import Logo from '../header/logo/logo.component';
-import environment from '../../../environment/environment';
+import environment from "../../../environment/environment";
+import Logo from "../header/logo/logo.component";
+import Social from "../header/social/social.component";
 
 const Footer = () => {
   return (
     <>
       <MediaQuery maxWidth={1024}>
-        <footer className='footer'>
-          <div className='footer__wrapper'>
+        <footer className="footer">
+          <div className="footer__wrapper">
             <Logo />
-            <div className='footer__socials'>
+            <div className="footer__socials">
               <Social />
               <a
                 href={environment.discord}
-                className='footer__button'
-                target='_blank'
-                rel='noreferrer'
+                className="footer__button"
+                target="_blank"
+                rel="noreferrer"
               >
-                <h4 className='footer__button-title'>
-                  Join discord
-                </h4>
+                <h4 className="footer__button-title">Join discord</h4>
               </a>
             </div>
-            <div className='footer__legal'>
-              <p className='footer__legal-rights'>
-                2022 Larry. All rights reserved
+            <div className="footer__legal">
+              <p className="footer__legal-rights">
+                © 2023 Larry. All rights reserved
               </p>
             </div>
           </div>
         </footer>
       </MediaQuery>
       <MediaQuery minWidth={1025}>
-        <footer className='footer'>
-          <div className='footer__wrapper'>
-            <div className='footer__legal'>
+        <footer className="footer">
+          <div className="footer__wrapper">
+            <div className="footer__legal">
               <Logo />
-              <p className='footer__legal-rights'>
+              <p className="footer__legal-rights">
                 2022 Larry, The Adventurer. All rights reserved
               </p>
             </div>
-            <div className='footer__socials'>
+            <div className="footer__socials">
               <Social />
             </div>
           </div>
