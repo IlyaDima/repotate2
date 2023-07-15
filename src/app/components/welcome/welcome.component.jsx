4c088f1d2b2cfc4f77aa3e367ca72b08df62094f
id-webdev <ilyuha45@gmail.com>
2023-07-15 22:25:58 +0200

    feat: implement mobile version for welcome section and remove animations

--- src/app/components/welcome/welcome.component.jsx
+++ src/app/components/welcome/welcome.component.jsx
@@ -1,7 +1,5 @@
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 import './welcome.styles.scss';
 
@@ -16,141 +14,21 @@ Welcome.propTypes = {
 };
 
 export default function Welcome() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const armRef = useRef(null);
-  const frameRef = useRef(null);
-  const larryRef = useRef(null);
-  const officeRef = useRef(null);
-  const tableRef = useRef(null);
-  const textBubbleRef = useRef(null);
-
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: frameRef.current,
-          pin: true,
-          start: '-=25',
-          end: 'center',
-          // markers: true,
-          id: 'welcome-1',
-        },
-        ease: 'power2',
-      });
-
-      tl.fromTo(
-        officeRef.current,
-        {
-          scale: 2,
-          filter: 'brightness(.5)',
-        },
-        {
-          scale: 1,
-          filter: 'brightness(1)',
-        }
-      )
-        .to(
-          tableRef.current,
-          {
-            y: '-31.5vw',
-          },
-          '<'
-        )
-        .fromTo(
-          larryRef.current,
-          {
-            x: '30.5vw',
-          },
-          {
-            y: '-39vw',
-          },
-          '<25%'
-        );
-
-      // Second half of the animation
-      const tl2 = gsap.timeline({
-        scrollTrigger: {
-          trigger: frameRef.current,
-          pin: true,
-          start: '-=24',
-          end: 'center',
-          //markers: true,
-          id: 'welcome-2',
-        },
-        ease: 'power2',
-      });
-      tl2
-        .to(larryRef.current, {
-          rotate: -10,
-          scale: 1.4,
-          x: '6vw',
-          y: '-37vw',
-        })
-        .to(
-          tableRef.current,
-          {
-            scale: 1.5,
-            x: '-25.25vw',
-            y: '-34vw',
-          },
-          '<'
-        )
-        .to(
-          officeRef.current,
-          {
-            scale: 1.375,
-            filter: 'blur(2px)',
-          },
-          '<'
-        )
-        .fromTo(
-          armRef.current,
-          {
-            rotate: 45,
-          },
-          {
-            rotate: 0,
-            x: '-44vw',
-            y: '-34.5vw',
-          },
-          '<+=25%'
-        )
-        .fromTo(
-          textBubbleRef.current,
-          {
-            opacity: 0,
-            scale: 0,
-          },
-          {
-            opacity: 1,
-            scale: 1,
-          },
-          '<'
-        );
-    });
-
-    return () => ctx.revert();
-  }, []);
-
   return (
     <section className="welcome">
-      <div className="frame" ref={frameRef}>
-        <img className="office" ref={officeRef} src={office} alt="" />
-        <img className="larry" ref={larryRef} src={larry} alt="" />
-        <img className="arm" ref={armRef} src={arm} alt="" />
-        <img className="table" ref={tableRef} src={table} alt="" />
+      <div className="frame">
+        <img className="office" src={office} alt="" />
+        <img className="larry" src={larry} alt="" />
+        <img className="arm" src={arm} alt="" />
+        <img className="table" src={table} alt="" />
         <div
           className="text-bubble"
-          ref={textBubbleRef}
           style={{ backgroundImage: `url(${textBubble})` }}
         >
           <p>
-            Good day.
+            GM Travellers
             <br />
-            My name is Larry!
-            <br />
-            How are you?
+            How are you doing?
           </p>
         </div>
       </div>
